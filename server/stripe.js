255f4239b4d78ae28f988777a9b96204ebf2d47a
NurRydberg <nur.rydberg@gmail.com>
2024-04-02 12:42:48 +0200

    stripe init

---
+++ server/stripe.js
@@ -0,0 +1,12 @@
+const Stripe = require('stripe')
+
+const initStripe = () => {
+    const apiKey = process.env.STRIPE_KEY
+    if (!apiKey) {
+        throw new Error('Stripe API key not found')
+    } return new Stripe(apiKey, {
+        apiVersion: '2023-10-16',
+    })
+}
+
+module.exports = initStripe
--- server/stripe/stripe.controller.js
+++ server/stripe/stripe.controller.js
@@ -1,5 +1,6 @@
 const createCheckOutSession = (req, res) => {
     console.log("Creating checkout session")
+    res.json({ message: "Creating checkout session" })
 
 }
 
