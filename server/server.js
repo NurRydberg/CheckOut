3d0a57db1596293881decc17879214c63e04461c
NurRydberg <nur.rydberg@gmail.com>
2024-04-02 15:07:26 +0200

    auth added

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 
 
 const userRouter = require('./resources/users/users.router')
+const authRouter = require('./resources/auth/auth.router')
 
 const stripeRouter = require('./stripe/stripe.router')
 
@@ -12,6 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use("/api/users", userRouter);
+app.use("/api/auth", authRouter);
 
 app.use("/payments", stripeRouter)
 
