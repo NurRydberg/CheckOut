23ca2dacc82fd5d063e597fd80062b3f29178a87
NurRydberg <nur.rydberg@gmail.com>
2024-04-03 11:07:35 +0200

    logout on client done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [user, setUser] = useState<string>("");
+
+  useEffect(() => {
+    const authorize = async () => {
+      const response = await fetch("http://localhost:3001/api/auth/authorize", {
+        credentials: "include"
+      });
+      const data = await response.json();
+      if (response.status === 200) {
+        setUser(data);
+      } else {
+        setUser("");
+      }
+    }
+    authorize();
+  }, []);
 
   const register = async () => {
     const response = await fetch("http://localhost:3001/api/auth/register", {
@@ -30,10 +45,44 @@ const App = () => {
     console.log(data);
   };
 
+  const login = async () => {
+    const response = await fetch("http://localhost:3001/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email: "fakeemail@email.com",
+        password: "123456789",
+      }),
+    });
+    const data = await response.json();
+
+    if (response.status === 200) {
+      setUser(data);
+    } else {
+      setUser("");
+    }
+  };
+
+  const logout = async () => {
+    const response = await fetch("http://localhost:3001/api/auth/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (response.status === 200) {
+      setUser("");
+    }
+  }
+
   return (
     <>
       <h1>{user ? "Inloggad:" + user : "Utloggad"} </h1>
       <button onClick={register}>Registrera</button>
+      <button onClick={login}>Logga in</button>
+      <button onClick={logout}>Logga ut</button>
 
       <button onClick={handlePayment}>Show me the money</button>
     </>
--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const stripeRouter = require('./stripe/stripe.router')
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json())
 
 app.use(cookieSession({
